eb18d3757b3983586383fe62a7fc7c395c056edd
IssyDugg <91335574+IssyDugg@users.noreply.github.com>
2022-10-26 14:47:18 +0100

    1st commit

---
+++ src/components/Info-box-left/Info-box-left.compnents.jsx
@@ -0,0 +1,11 @@
+
+import React, { useState, useEffect } from "react";
+
+
+// return left info box 
+const leftInfoBox = () => {
+
+
+
+
+}
---
+++ src/components/Map-box-right/Map-box-right.jsx
@@ -0,0 +1,11 @@
+
+import React, { useState, useEffect } from "react";
+
+//hello
+// return left info box 
+const rightMapBox = () => {
+    
+
+
+
+}
